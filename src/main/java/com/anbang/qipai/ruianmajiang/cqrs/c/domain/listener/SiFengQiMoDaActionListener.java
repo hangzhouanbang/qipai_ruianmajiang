884a4462c932fcb59f7c1e0f124ef7b3ebc54c02
shendewei <1361959119@qq.com>
2018-09-26 16:45:03 +0800

    四风齐

--- src/main/java/com/anbang/qipai/ruianmajiang/cqrs/c/domain/listener/SiFengQiMoDaActionListener.java
+++ src/main/java/com/anbang/qipai/ruianmajiang/cqrs/c/domain/listener/SiFengQiMoDaActionListener.java
@@ -1,4 +1,4 @@
-package com.anbang.qipai.ruianmajiang.cqrs.c.domain;
+package com.anbang.qipai.ruianmajiang.cqrs.c.domain.listener;
 
 import com.dml.majiang.ju.Ju;
 import com.dml.majiang.pai.MajiangPai;
@@ -10,6 +10,7 @@ import com.dml.majiang.player.action.listener.mo.MajiangPlayerMoActionStatistics
 import com.dml.majiang.player.action.mo.MajiangMoAction;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,6 +20,7 @@ import java.util.Map;
 public class SiFengQiMoDaActionListener implements MajiangPlayerDaActionStatisticsListener, MajiangPlayerMoActionStatisticsListener {
 
     private Map<String, Map<MajiangPai, Integer>> playerFengCounter = new HashMap<>();
+    private Map<String, Boolean> firstMoPaiMap = new HashMap<>();
 
     private Map<MajiangPai, Integer> getOrCreateFengpaiCounter(final String playerId) {
         Map<MajiangPai, Integer> fengPaiCounter = this.playerFengCounter.get(playerId);
@@ -51,26 +53,47 @@ public class SiFengQiMoDaActionListener implements MajiangPlayerDaActionStatisti
     @Override
     public void updateForNextPan() {
         this.playerFengCounter.clear();
+        this.firstMoPaiMap.clear();
     }
 
+    //用户是不是第一次摸排
+    private boolean isPlayerFistMo(String playerId){
+        Boolean mo = this.firstMoPaiMap.putIfAbsent(playerId, Boolean.TRUE);
+        return mo == null;
+    }
 
     @Override
     public void update(MajiangMoAction moAction, Ju ju) throws Exception {
+        final String playerId = moAction.getActionPlayerId();
         final Pan currentPan = ju.getCurrentPan();
-        final MajiangPlayer player = currentPan.findPlayerById(moAction.getActionPlayerId());
+        final MajiangPlayer player = currentPan.findPlayerById(playerId);
+        //第一次摸排，遍历初始牌
+        if (isPlayerFistMo(playerId)){
+            final List<MajiangPai> fangruShoupaiList = player.getFangruShoupaiList();
+            for (MajiangPai pai : fangruShoupaiList) {
+                //只记录风牌
+                if (isFengPai(pai)) {
+                    this.moFengPai(pai,playerId);
+                }
+            }
+        }
         final MajiangPai pai = player.getGangmoShoupai();
         //只记录风牌
         if (isFengPai(pai)) {
-            final String playerId = player.getId();
-            final Map<MajiangPai, Integer> fengPaiCounter = this.getOrCreateFengpaiCounter(playerId);
-            Integer counter = fengPaiCounter.get(pai);
-            if (counter == null) {
-                fengPaiCounter.put(pai, 1);
-            } else {
-                fengPaiCounter.put(pai, ++counter);
-            }
+            this.moFengPai(pai,playerId);
         }
     }
+
+    private void moFengPai(final MajiangPai pai,final String playerId){
+        final Map<MajiangPai, Integer> fengPaiCounter = this.getOrCreateFengpaiCounter(playerId);
+        Integer counter = fengPaiCounter.get(pai);
+        if (counter == null) {
+            fengPaiCounter.put(pai, 1);
+        } else {
+            fengPaiCounter.put(pai, ++counter);
+        }
+    }
+
 
     public boolean couldSiFengQi(final String playerId){
         final Map<MajiangPai, Integer> fengPaiCounter = this.getOrCreateFengpaiCounter(playerId);
