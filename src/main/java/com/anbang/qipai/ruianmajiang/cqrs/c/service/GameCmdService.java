a05e16d6b3ad502a679a5294b656ac8b82535e8e
Administrator <Administrator@WIN-OKH646EVNPT>
2018-09-27 09:04:59 +0800

    离开相关修改

--- src/main/java/com/anbang/qipai/ruianmajiang/cqrs/c/service/GameCmdService.java
+++ src/main/java/com/anbang/qipai/ruianmajiang/cqrs/c/service/GameCmdService.java
@@ -23,4 +23,9 @@ public interface GameCmdService {
 	MajiangGameValueObject voteToFinish(String playerId, Boolean yes) throws Exception;
 
 	MajiangGameValueObject finishGameImmediately(String gameId) throws Exception;
+
+	MajiangGameValueObject leaveGameByOffline(String playerId) throws Exception;
+
+	MajiangGameValueObject leaveGameByHangup(String playerId) throws Exception;
+
 }
