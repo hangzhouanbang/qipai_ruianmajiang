809d7dd303e34154128b4f773d9a1b74f9540912
林少聪 < @PC-20180515PRDG>
2018-10-30 19:17:18 +0800

    游戏结束删除游戏

--- src/main/java/com/anbang/qipai/ruianmajiang/web/controller/GameController.java
+++ src/main/java/com/anbang/qipai/ruianmajiang/web/controller/GameController.java
@@ -363,7 +363,6 @@ public class GameController {
 			MajiangGameDbo majiangGameDbo = majiangGameQueryService.findMajiangGameDboById(gameId);
 			JuResultVO juResult = new JuResultVO(juResultDbo, majiangGameDbo);
 			ruianMajiangResultMsgService.recordJuResult(juResult);
-			gameMsgService.gameFinished(gameId);
 		}
 
 		if (majiangGameValueObject.getState().name().equals(FinishedByVote.name)
@@ -423,6 +422,9 @@ public class GameController {
 			MajiangGameDbo majiangGameDbo = majiangGameQueryService.findMajiangGameDboById(gameId);
 			JuResultVO juResult = new JuResultVO(juResultDbo, majiangGameDbo);
 			ruianMajiangResultMsgService.recordJuResult(juResult);
+		}
+		if (majiangGameValueObject.getState().name().equals(FinishedByVote.name)
+				|| majiangGameValueObject.getState().name().equals(Canceled.name)) {
 			gameMsgService.gameFinished(gameId);
 		}
 
