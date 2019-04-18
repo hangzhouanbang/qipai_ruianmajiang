e532f1b297f066e24d95a78ed9e862e23f7a49de
林少聪 < @PC-20180515PRDG>
2019-04-18 16:56:54 +0800

    bugfix

--- src/main/java/com/anbang/qipai/ruianmajiang/msg/receiver/GameRoomMsgReceiver.java
+++ src/main/java/com/anbang/qipai/ruianmajiang/msg/receiver/GameRoomMsgReceiver.java
@@ -62,10 +62,13 @@ public class GameRoomMsgReceiver {
 					} else {
 						MajiangGameValueObject gameValueObject = gameCmdService.finishGameImmediately(gameId);
 						majiangGameQueryService.finishGameImmediately(gameValueObject);
-						JuResultDbo juResultDbo = majiangPlayQueryService.findJuResultDbo(gameId);
-						MajiangHistoricalJuResult juResult = new MajiangHistoricalJuResult(juResultDbo, majiangGameDbo);
-						ruianMajiangResultMsgService.recordJuResult(juResult);
 						ruianMajiangGameMsgService.gameFinished(gameId);
+						JuResultDbo juResultDbo = majiangPlayQueryService.findJuResultDbo(gameId);
+						if (juResultDbo != null) {
+							MajiangHistoricalJuResult juResult = new MajiangHistoricalJuResult(juResultDbo,
+									majiangGameDbo);
+							ruianMajiangResultMsgService.recordJuResult(juResult);
+						}
 					}
 				} catch (Throwable e) {
 					e.printStackTrace();
